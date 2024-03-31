49bddea26ae82ac8c6fba0728798b1df7b674b1f
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-03-31 11:33:49 +0530

    Create user model

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 mongoose
-.connect( process.env.MONGO /* npm i dotenv */)
+.connect( process.env.MONGO )
 .then(
   () => {console.log('mongodb is connected..');}
 )
