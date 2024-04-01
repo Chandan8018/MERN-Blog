132abf40688c8c6533a4add22ef13c450c8c3d3d
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-04-01 11:24:11 +0530

    Create UI of SignUp

--- MERN_Blog/src/components/Header.jsx
+++ MERN_Blog/src/components/Header.jsx
@@ -1,50 +1,55 @@
-import React from 'react'
-import {Button, Navbar, TextInput} from 'flowbite-react'
+import React from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import { FcSearch } from "react-icons/fc";
 import { FaMoon } from "react-icons/fa";
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from "react-router-dom";
 export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar>
-      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 rounded-lg text-white'>ThinkBig's</span>
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 rounded-lg text-white">
+          ThinkBig's
+        </span>
         Blog
       </Link>
       <form>
-        <TextInput 
-        type='text' 
-        placeholder='Search...'
-        rightIcon={FcSearch}
-        className='hidden lg:inline text-2xl'
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={FcSearch}
+          className="hidden lg:inline text-2xl"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-        <FcSearch className='text-2xl'/>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <FcSearch className="text-2xl" />
       </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-         <FaMoon/>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' color='gray' pill outline>
-         Sign In
-        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" color="gray" pill outline>
+            Sign In
+          </Button>
         </Link>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
       </div>
 
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
--- MERN_Blog/src/pages/Signup.jsx
+++ MERN_Blog/src/pages/Signup.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left div */}
+        <div className="flex-1">
+          <Link to="/" className=" font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 rounded-lg text-white">
+              ThinkBig's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            You can sign up with your email and password or with Google.
+          </p>
+        </div>
+
+        {/* right div */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-3">
+            <Label value="Your username"/>
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+              />
+            <Label value="Your email"/>  
+              <TextInput
+                type="text"
+                placeholder="name@Company.com"
+                id="email"
+              />
+            <Label value="Your password"/>  
+              <TextInput
+                type="text"
+                placeholder="Password"
+                id="password"
+              />
+            <Button type="submit" gradientDuoTone='purpleToPink'>
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
